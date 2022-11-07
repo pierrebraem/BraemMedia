0b5ff7701355655a4f6b25e810745922d0f06016
Pierre Braem <pierrebraem@orange.fr>
2022-11-07 11:22:40 +0100

    FEAT: Interface de base de la page d'accueil

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,51 @@
 import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build (BuildContext context){
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'APIFilm',
+      home: Accueil(),
+    );
+  }
+}
+
+class Accueil extends StatefulWidget{
+  const Accueil({Key? key}) : super(key: key);
+
+  @override
+  _AccueilState createState() => _AccueilState();
+}
+
+class _AccueilState extends State<Accueil>{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("APIFilm"),
+        automaticallyImplyLeading: false,
+        centerTitle: true,
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            margin: const EdgeInsets.fromLTRB(16, 16, 16, 16),
+            child: TextField(
+              decoration: InputDecoration(
+                prefixIcon: const Icon(Icons.search),
+                hintText: 'Nom du film',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: const BorderSide(color: Colors.black),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
