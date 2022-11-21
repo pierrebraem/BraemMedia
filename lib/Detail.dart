30cab3c83a51343a8be45783052ad4579ecb7235
Pierre Braem <pierrebraem@orange.fr>
2022-11-21 09:40:42 +0100

    UPDATE: Page detail type terminé

--- lib/Detail.dart
+++ lib/Detail.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'Film.dart';
 
 class Detail extends StatelessWidget{
   const Detail({super.key});
@@ -10,11 +9,17 @@ class Detail extends StatelessWidget{
       appBar: AppBar(
         title: const Text('Detail'),
       ),
-      body: Column(
+      body: ListView(
         children: <Widget>[
           Container(
-            child: Text('Coucou'),
+            padding: (
+                EdgeInsets.only(left: 20, top: 20)
+            ),
           ),
+          Text('Smile', textAlign: TextAlign.center, style: TextStyle(color: Colors.cyan, fontSize: 40.0)),
+          Text('2022', textAlign: TextAlign.center, style: TextStyle(color: Colors.cyan)),
+          Image.network('https://m.media-amazon.com/images/M/MV5BZjE2ZWIwMWEtNGFlMy00ZjYzLWEzOWEtYzQ0MDAwZDRhYzNjXkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_SX300.jpg', width: 300, height: 300),
+          Text('Une description', textAlign: TextAlign.center, style: TextStyle(color: Colors.cyan)),
         ],
       ),
     );
