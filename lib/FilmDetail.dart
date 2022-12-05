29f13585501cd3fc56cd2ebc612262ced60a0732
Pierre Braem <pierrebraem@orange.fr>
2022-12-05 16:35:56 +0100

    FEAT: Création de la classe "FilmDetail"

---
+++ lib/FilmDetail.dart
@@ -0,0 +1,49 @@
+class FilmDetail{
+  final String title;
+  final String urlImage;
+  final String rated;
+  final String released;
+  final String runtime;
+  final String genre;
+  final String director;
+  final String writer;
+  final String actors;
+  final String plot;
+  final String metascore;
+  final String dvd;
+  final String boxoffice;
+
+  const FilmDetail({
+    required this.title,
+    required this.urlImage,
+    required this.rated,
+    required this.released,
+    required this.runtime,
+    required this.genre,
+    required this.director,
+    required this.writer,
+    required this.actors,
+    required this.plot,
+    required this.metascore,
+    required this.dvd,
+    required this.boxoffice,
+  });
+
+  factory FilmDetail.fromJson(Map<String, dynamic> json){
+    return FilmDetail(
+      title: json['Title'],
+      urlImage : json['Poster'],
+      rated: json['Rated'],
+      released: json['Released'],
+      runtime: json['Runtime'],
+      genre: json['Genre'],
+      director: json['Director'],
+      writer: json['Writer'],
+      actors: json['Actors'],
+      plot: json['Plot'],
+      metascore: json['Metascore'],
+      dvd: json['DVD'],
+      boxoffice: json['BoxOffice'],
+    );
+  }
+}
