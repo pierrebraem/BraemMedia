f4176cf047faa6f4828af5e8e3dea18a353fca90
Pierre Braem <pierrebraem@orange.fr>
2022-12-06 10:56:40 +0100

    UPDATE: Ajoute l'information "imdbID"

--- lib/Film.dart
+++ lib/Film.dart
@@ -1,10 +1,12 @@
 class Film{
   final String title;
+  final String imdbID;
   final String urlImage;
   final String annee;
 
   const Film({
     required this.title,
+    required this.imdbID,
     required this.urlImage,
     required this.annee,
 });
@@ -12,6 +14,7 @@ class Film{
   factory Film.fromJson(Map<String, dynamic> json){
     return Film(
       title: json['Title'],
+      imdbID: json['imdbID'],
       urlImage: json['Poster'],
       annee: json['Year'],
     );
