47c5f3c6ec1a58ffe2a12100aa395e5d9e77a94d
Pierre Braem <pierrebraem@orange.fr>
2024-02-23 14:22:35 +0100

    Design basique de la recherche

--- lib/tabs/rechercher.dart
+++ lib/tabs/rechercher.dart
@@ -1,12 +1,33 @@
 import 'package:flutter/material.dart';
 
-class RechercherPage extends StatelessWidget{
+class RechercherPage extends StatefulWidget{
   const RechercherPage({super.key});
 
   @override
+  State<RechercherPage> createState() => _RechercherPageState();
+}
+
+class _RechercherPageState extends State<RechercherPage>{
+  @override
   Widget build(BuildContext context){
-    return const Center(
-      child: Text("Page Rechercher")
+    return Scaffold(
+      body: Column(
+        children: [
+          TextField(
+            decoration: InputDecoration(
+              prefixIcon: const Icon(Icons.search),
+              hintText: 'Entrez le nom d\'un média',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20),
+                borderSide: const BorderSide(color: Colors.black)
+              )
+            )
+          ),
+          const ElevatedButton(
+            onPressed: null, child: Text('Rechercher')
+          )
+        ]
+      )
     );
   }
 }
